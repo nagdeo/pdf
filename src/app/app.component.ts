cb0051b64b2d567c09b9624784509cc145da6aca
simmi23 <simminagdeo@gmail.com>
2020-06-10 15:40:18 +0530

    pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdf';
+  contentDes:any ='';
+  downloadPdf(){
+    var doc = new jsPDF();
+    var splitTitle = doc.splitTextToSize(this.contentDes, 270);
+    var pageHeight = doc.internal.pageSize.height;
+    doc.setFontType("normal");
+    doc.setFontSize(11);
+    var y = 7;
+    for (var i = 0; i < splitTitle.length; i++) {                
+        if (y > 280) {
+            y = 10;
+            doc.addPage();
+        }
+        doc.text(15, y, splitTitle[i]);
+        y = y + 7;
+    }
+    doc.save('my.pdf'); 
+  }
 }
